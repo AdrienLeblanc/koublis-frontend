5d23ab393027757e87fba22b755e1348eda770e5
Adrien Leblanc <adrienleblanc53@gmail.com>
2025-05-26 21:16:13 +0200

    config: add koublis api dependency from local npm repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  AuthControllerService,
+  WineControllerService,
+} from '@koublis/api-client';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'koublis-frontend';
+
+  constructor(
+    authControllerService: AuthControllerService,
+    wineControllerService: WineControllerService,
+  ) {}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApiModule, Configuration } from '@koublis/api-client';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(
+      ApiModule.forRoot(
+        () =>
+          new Configuration({
+            basePath: 'https://votre-api-url.com',
+          }),
+      ),
+    ),
+  ],
 };
