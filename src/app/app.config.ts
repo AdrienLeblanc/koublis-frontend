bcdcafd17ce58120e23f98131a1aa7fe7f864668
Adrien Leblanc <adrienleblanc53@gmail.com>
2025-05-28 00:14:58 +0200

    config: add environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,25 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 import { ApiModule, Configuration } from '@koublis/api-client';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     importProvidersFrom(
       ApiModule.forRoot(
         () =>
           new Configuration({
-            basePath: 'https://votre-api-url.com',
+            basePath: environment.apiBasePath,
           }),
       ),
     ),
